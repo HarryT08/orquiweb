c7afc367e1c2b531ff84b8ba89118c28b49f3e5f
Felipe Alferez <alferezfelipe@gmail.com>
2022-06-14 02:56:33 -0500

    cambio en las mesas, ya sirve el click en todas

--- js/prueba.js
+++ js/prueba.js
@@ -1,32 +1,28 @@
-const body = document.getElementById('view')
-
-function loadData(){
-    for(let i = 0; i<5; i++){
-        const row = document.createElement("tr");
-        for(let j = 0; j < 4; j++){
-            let id = `${i*4 + j + 1}`
-            const mesita = document.createElement("td");
-            const contenedorMesa = document.createElement("div")
-            const btnMesa = document.createElement("button")
-            btnMesa.innerHTML = id
-            btnMesa.setAttribute("id", 'boton'+id);
-            contenedorMesa.setAttribute("id", 'mesa'+id);
-            contenedorMesa.appendChild(btnMesa)
-            mesita.appendChild(contenedorMesa)
-            row.appendChild(mesita)
-        }
-        body.appendChild(row)
+function loadData() {
+  const table = document.getElementById("view");
+  for (let i = 0; i < 5; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < 4; j++) {
+      let id_mesa = `${i * 4 + j + 1}`;
+      const mesa = document.createElement("td");
+      const btnMesa = document.createElement("button");
+      btnMesa.innerHTML = id_mesa;
+      btnMesa.id = `boton${id_mesa}`
+      btnMesa.onclick = cambiarEstado
+      mesa.appendChild(btnMesa);
+      row.appendChild(mesa);
     }
+    table.appendChild(row);
+  }
 }
 
-document.addEventListener("DOMContentLoaded",loadData())
+function cambiarEstado() {
+  const mesa = document.getElementById(this.id);
+  if (mesa.classList.contains("ocupado")) {
+    mesa.classList.remove("ocupado");
+  } else {
+    mesa.classList.add("ocupado");
+  }
+}
 
-document.getElementById('boton5').addEventListener('click' , ()=>{
-    // alert(document.getElementById('mesa5').classList === undefined)
-    if(document.getElementById('mesa5').classList.contains('ocupado')){
-        document.getElementById('mesa5').classList.remove('ocupado');
-    }
-    else{
-        document.getElementById('mesa5').classList.add('ocupado');
-    }
-})
+document.addEventListener("DOMContentLoaded", loadData());
