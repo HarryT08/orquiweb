de7c8f0936152870a4e7f4e37b77ade7e75cb7ac
Harold Rueda <haroldrant@ufps.edu.co>
2022-06-15 16:08:59 -0500

    Añadida conexión a base de datos hostinger

--- BD/bd_orquiweb.sql
+++ BD/bd_orquiweb.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE 'bd_orquiweb'
-
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
 const express = require('express');
 const path = require('path');
 const app = express()
+const mysql = require('mysql');
 
 //settings
 app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname , 'views'));
 
+//Cochinada
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'sql565.main-hosting.eu',
+    user: 'u173148296_root',
+    password: 'Root1506',
+    port: '3306',
+    database: 'u173148296_bd_orquiweb',
+    debug: false
+});
+
+pool.getConnection(function(err, connection) {
+
+    // do whatever you want with your connection here
+
+    connection.release();
+
+});
+
+//Middlewares
+// app.use(myConnection(mysql,{
+//     host: 'sql565.main-hosting.eu',
+//     user: 'u173148296_root',
+//     password: 'Root1506',
+//     port: '3306',
+//     database: 'u173148296_bd_orquiweb'
+// }, 'single'));
+
+//Archivos fijos
 app.use(express.static(path.join(__dirname, 'public')))
+
+//Routing
+app.get('/' , (req, res) => {
+    res.render('index')
+});
 
 app.listen(app.get('port') , () => {
     console.log(__dirname);
     console.log( `Sever on port ${app.get('port')}` );
-})
-
-app.get('/' , (req, res) => {
-    res.render('index')
 })
