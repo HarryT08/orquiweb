c335849d9f338828ca8158772c1d6131586cfedc
Felipe Alferez <alferezfelipe@gmail.com>
2022-06-16 18:48:58 -0500

    agregado usuarios.ejs

--- src/modulos/routes/index.js
+++ src/modulos/routes/index.js
@@ -4,9 +4,10 @@ const usersRouter = require('./usersRouter');
 function routerApi(app){
     //add new route for versions
     const router = express.Router();
+    
     app.use('/api', router);
     //use route father
-    router.use('/users', usersRouter);
+    router.use('/home', usersRouter);
 }
 
 module.exports = routerApi;
