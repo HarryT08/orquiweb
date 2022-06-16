d05d07f254ee6f5ece2652e4135a7efcaafaba5c
Harold Rueda <haroldrant@ufps.edu.co>
2022-06-16 18:49:34 -0500

    Commit chimbita sog

--- src/modulos/routes/index.js
+++ src/modulos/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usersRouter = require('./usersRouter');
+const adminRouter = require('./adminRouter');
 
 function routerApi(app){
     //add new route for versions
@@ -7,6 +8,7 @@ function routerApi(app){
     app.use('/api', router);
     //use route father
     router.use('/users', usersRouter);
+    router.use('/admin', adminRouter);
 }
 
 module.exports = routerApi;
