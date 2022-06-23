2117c5cfdc202906b38c9a2f9946683c76a33a5a
Harold Rueda <haroldrant@ufps.edu.co>
2022-06-23 06:56:14 -0500

    fix: password and username

--- js/login.js
+++ js/login.js
@@ -1,35 +1,39 @@
 const API_URL = "https://orquiweb.herokuapp.com";
 
-$('#form-login').on('submit', function(e){    
+$('#form-login').on('submit', function (e) {
     //Prevenimos envio de formulario
     e.preventDefault();
     //Obtenemos los valores del formulario
-    var data = {
-        username: document.getElementById('username').value,
-        password: document.getElementById('password').value
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    if (username && password) {
+        var data = {
+            username: username,
+            password: password
+        }
+        //Realizamos el envío por fetch a la API
+        fetch(`${API_URL}/home`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (result) {
+                    window.location.href = `./views/home_${result.rol}`;
+                    sessionStorage.setItem('user', result.username.toUpperCase());
+                    sessionStorage.setItem('rol', result.rol.toUpperCase());
+                    sessionStorage.setItem('nombre', result.nombre + " " + result.apellido);
+                    sessionStorage.setItem('userId', result.idUsuario);
+                });
+            } else {
+                response.json().then(function (result) {
+                    document.getElementById('error').innerHTML = result.msg;
+                });
+            }
+        })
     }
-    //Realizamos el envío por fetch a la API
-    fetch(`${API_URL}/home`,{
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function(response){
-        if(response.ok){
-            response.json().then(function(result){
-                window.location.href = `./views/home_${result.rol}`;
-                sessionStorage.setItem('user', result.username.toUpperCase());
-                sessionStorage.setItem('rol', result.rol.toUpperCase());
-                sessionStorage.setItem('nombre', result.nombre + " " + result.apellido);
-                sessionStorage.setItem('userId', result.idUsuario);
-            }); 
-        }else{
-            response.json().then(function(result){
-                document.getElementById('error').innerHTML = result.msg;
-            });            
-        }
-    })
 });
 
 localStorage.clear();
